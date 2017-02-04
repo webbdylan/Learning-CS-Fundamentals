35d7414b7b16ab38c2162087b75c36ff91be39e1
iAmWillShepherd <williamrshepherd@gmail.com>
2017-02-04 13:30:58 -0600

    Provide correct intent for method

--- data_structures/bag/src/IBagADT.cs
+++ data_structures/bag/src/IBagADT.cs
@@ -11,7 +11,7 @@ public interface IBagADT<T> : IEnumerable<T>
     //Checks to see if the container contains any items
     bool IsEmpty();
 
-    //Counts the number of items the container can hold
+    //Counts the number of items in the container
     int Size();
 
     //Returns a string representation of all the items in the container
