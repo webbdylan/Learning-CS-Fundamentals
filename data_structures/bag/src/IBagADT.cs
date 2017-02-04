271250e6e842f2a6e0e1dd4c64ce83857aead09c
iAmWillShepherd <williamrshepherd@gmail.com>
2017-02-04 02:21:09 -0600

    Add interface for ADT

---
+++ data_structures/bag/src/IBagADT.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+
+public interface IBagADT<T> : ICollection<T>, IEnumerable<T>
+{
+    //Adds an item to the container
+    new void Add(T item);
+
+    //Removes the first matching item from the container
+    new void Remove(T item);
+
+    //Removes all items from the container
+    void Clean();
+
+    //Checks to see if the container contains any items
+    bool IsEmpty();
+
+    //Counts the number of items the container can hold
+    int Size();
+
+    //Returns a string representation of all the items in the container
+    string Peek();
+}
