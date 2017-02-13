b3d21224fdc2922727164952548e8860297bfa30
Dylan Webb <webb.dylan@gmail.com>
2017-02-13 07:08:08 -0600

    partly finished w/stack

--- data_structures/src/Stack/Stack.cs
+++ data_structures/src/Stack/Stack.cs
@@ -6,24 +6,33 @@ namespace ADT
 {
     public class Stack<T> : IStack<T>
     {
+        private T[] _stack { get; set; }
+
         public Stack(int size)
         {
-
+            _stack = new T[size]; 
         }
 
         public IEnumerator<T> GetEnumerator()
         {
-            throw new NotImplementedException();
+             return (IEnumerator<T>)_stack.GetEnumerator(); 
         }
 
         public bool IsEmpty()
         {
-            throw new NotImplementedException();
+            return this.Size() > 0 ? false : true;
         }
 
         public T Peek()
         {
-            throw new NotImplementedException();
+            if(!this.IsEmpty())
+            {
+                return _stack[0];
+            }
+            else
+            {
+                throw new Exception("Stack is empty- Cannot peek.");
+            }
         }
 
         public T Pop()
@@ -38,12 +47,12 @@ namespace ADT
 
         public int Size()
         {
-            throw new NotImplementedException();
+            return _stack.Length;
         }
 
         IEnumerator IEnumerable.GetEnumerator()
         {
-            throw new NotImplementedException();
+            return _stack.GetEnumerator();
         }
     }
 }
